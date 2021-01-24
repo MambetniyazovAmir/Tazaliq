75b8976c571dc0d2eccb38050e1f4c608ae9cefe
MambetniyazovAmir <mr.amirniyaz@mail.ru>
2021-01-24 17:26:20 +0500

    editProf

---
+++ app/src/main/java/com/example/tazaliq/ui/edit_profile/EditProfileFragment.kt
@@ -0,0 +1,8 @@
+package com.example.tazaliq.ui.edit_profile
+
+import androidx.fragment.app.Fragment
+import com.example.tazaliq.R
+
+class EditProfileFragment : Fragment(R.layout.fragment_edit_profile) {
+
+}
--- app/src/main/java/com/example/tazaliq/ui/settings/SettingFragment.kt
+++ app/src/main/java/com/example/tazaliq/ui/settings/SettingFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.tazaliq.R
+import com.example.tazaliq.core.extentions.onClick
+import com.example.tazaliq.data.firebase.ProfileHelper
 import com.example.tazaliq.databinding.FragmentSettingsBinding
+import com.example.tazaliq.ui.main.MainFragmentDirections
 
 class SettingFragment : Fragment(R.layout.fragment_settings) {
 
@@ -17,6 +20,9 @@ class SettingFragment : Fragment(R.layout.fragment_settings) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSettingsBinding.bind(view)
         navController = Navigation.findNavController(view)
+        val action = MainFragmentDirections.actionMainFragmentToEditProfileFragment()
+        binding.tvEditProfile.onClick {
+            navController.navigate(action)
+        }
     }
-
 }
