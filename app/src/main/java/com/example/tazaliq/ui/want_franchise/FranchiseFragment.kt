8d731737bf4fa7584bb0b2564a1f54e479b0a05e
MambetniyazovAmir <mr.amirniyaz@mail.ru>
2021-02-13 16:36:29 +0500

    done

--- app/src/main/java/com/example/tazaliq/ui/want_franchise/FranchiseFragment.kt
+++ app/src/main/java/com/example/tazaliq/ui/want_franchise/FranchiseFragment.kt
@@ -1,15 +1,17 @@
 package com.example.tazaliq.ui.want_franchise
 
 import android.os.Bundle
+import android.telephony.SmsManager
 import android.view.View
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.tazaliq.R
+import com.example.tazaliq.core.BaseFragment
 import com.example.tazaliq.core.extentions.onClick
 import com.example.tazaliq.databinding.FragmentFranchiseBinding
 
-class FranchiseFragment : Fragment(R.layout.fragment_franchise) {
+
+class FranchiseFragment : BaseFragment(R.layout.fragment_franchise) {
 
     private lateinit var binding: FragmentFranchiseBinding
     private lateinit var navController: NavController
@@ -20,6 +22,20 @@ class FranchiseFragment : Fragment(R.layout.fragment_franchise) {
         navController = Navigation.findNavController(requireActivity(), R.id.root_nav_host)
         binding.btnBack.onClick {
             navController.popBackStack()
+        }
+        binding.btnSend.onClick {
+            sendSMS("+998913021226", binding.etName.text.toString() + "\n" + binding.etMessageText.text.toString() + "\n" + binding.tvCity.text)
+        }
+    }
+
+    private fun sendSMS(phoneNum: String, msg: String) {
+        try {
+            val smsManager: SmsManager = SmsManager.getDefault()
+            smsManager.sendTextMessage(phoneNum, null, msg, null, null)
+            toastLN("SMS send")
+        } catch (ex: Exception) {
+            toastLN(ex.message)
+            ex.printStackTrace()
         }
     }
 }
